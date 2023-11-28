80559ebbccea2b60dcea2a3f732ceedafb56c971
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2023-11-28 15:12:41 +0530

    "added some comments for better understanding"

--- demo/func3.go
+++ demo/func3.go
@@ -34,16 +34,18 @@ func Func3(ch1 chan ResultWithError, wg *sync.WaitGroup, m *sync.Mutex) {
 	respBody := result.RespBody
 	fmt.Println("Result in func3 after func4 is ", respBody)
 
-	// Transfer the value from ch2 to ch1
-
 	ch3 := make(chan ResultWithError)
 	wg2.Add(1)
 	go Func6(ch3, &wg2, m)
+	
 	result2, ok := <-ch3
+
+
 	if !ok {
 		fmt.Println("Channel closed in func3.")
 		return
 	}
+	wg2.Wait()
 	respBody2 := result2.RespBody
 	fmt.Println("Result in func3 after func6 is ", respBody2)
 	ch1 <- result2
--- demo/func6.go
+++ demo/func6.go
@@ -1,20 +1,26 @@
+// func6.go
 package demo
 
-// package main
-
 import (
+	"fmt"
 	"sync"
+	"time"
 )
 
-func Func6(ch1 chan ResultWithError, wg *sync.WaitGroup, m *sync.Mutex) {
-	m.Lock()
+func Func6(ch chan ResultWithError, wg *sync.WaitGroup, m *sync.Mutex) {
+
 	defer wg.Done()
 	println("Func6 reached")
-	a := 50
-	b := 70
-	res := a + b
-	print("res in func6 is ", res)
-	ch1 <- ResultWithError{RespBody: res, APIError: nil}
 
-	m.Unlock()
+	// Simulating some processing in Func6
+	time.Sleep(1 * time.Second)
+
+	// Performing some computation
+	result := 20
+
+	// Print the result in Func6
+	fmt.Printf("Result in Func6: %v\n", result)
+
+	// Send the result back to the calling function
+	ch <- ResultWithError{RespBody: result, APIError: nil}
 }
